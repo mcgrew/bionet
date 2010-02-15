f1c9ae1750bc861c1a7e2907446d69b153c3f1b2
Thomas McGrew <tmcgrew@purdue.edu>
2010-02-15 22:19:19 +0000

    Improved the calculation algorithm of ClusteredLayout

--- src/edu/purdue/jsysnet/ui/layout/ClusteredLayout.java
+++ src/edu/purdue/jsysnet/ui/layout/ClusteredLayout.java
@@ -2,6 +2,7 @@ package edu.purdue.jsysnet.ui.layout;
 
 import edu.purdue.jsysnet.util.Correlation;
 import edu.purdue.jsysnet.util.Molecule;
+import edu.purdue.jsysnet.util.PolarPoint2D;
 
 import java.awt.geom.Point2D;
 import java.awt.Dimension;
@@ -20,11 +21,12 @@ import org.apache.commons.collections15.Transformer;
  * @author Thomas McGrew
  * @version 1.0
  */
-public class ClusteredLayout extends AbstractLayout<Molecule,Correlation> {
+public class ClusteredLayout extends RandomLayout<Molecule,Correlation> {
 
 	protected int maxIterations;
 	protected int currentIteration;
-	protected int multiplier;
+	protected final double attractionStep = 0.2;
+	protected final double repulsionStep = 0.05;
 
 	/**
 	 * Constructor.
@@ -38,55 +40,37 @@ public class ClusteredLayout extends AbstractLayout<Molecule,Correlation> {
 	 * Initializes the location of the Graph vertices.
 	 */
 	public void initialize( ) {
-		this.multiplier = Math.max( this.size.height, this.size.width ) / 2;
-		this.maxIterations = this.multiplier * 2;
+		super.initialize( );
+		this.maxIterations = Math.max( this.size.height, this.size.width ) * 2;
 		this.currentIteration = 0;
-		// put all vertices somewhere random;
-		for ( Molecule vertex : this.graph.getVertices( )) {
-			this.locations.get( vertex ).setLocation( Math.random( ) * this.size.width, 
-				                                        Math.random( ) * this.size.height);
-		}
 		while ( !this.done( ))
 			this.step( );
 	}
 
 	public void step( ) {
-		Point2D v,w;
-		double x, y, r, theta, newR;
-		int compare;
-		boolean moved = true;
-		Molecule first;
-		moved = false;
-		Collection <Molecule> moleculeList = this.graph.getVertices( );
-		Correlation e;
-		for( Molecule m : moleculeList) {
-			for ( Molecule n : moleculeList ) {
-				e = m.getCorrelation( n );
-				if ( e == null )
-					continue;
-				newR = this.multiplier * ( 1.1 - (e.getValue( )));
-				// randomize the order of the vertices
-				v = this.locations.get( first = ( Math.random( ) < 0.5 ) ?  m : n );
-				w = this.locations.get( e.getOpposite( first ));
-				// calculate polar coordinates of w assuming origin of v
-				x = w.getX( ) - v.getX( );
-				y = w.getY( ) - v.getY( );
-				r = Math.hypot( x, y );
-				theta = Math.atan2( y, x );	
-				// move w to it's new location, slightly closer to where it should be.
-				compare = Double.compare( newR, r );
-				if ( compare != 0 ) {
-					moved = true;
-					x = v.getX( ) + ( r+compare ) * Math.cos( theta );
-					y = v.getY( ) + ( r+compare ) * Math.sin( theta );
-					// if the vertex goes off the screen, move it to the middle.
-					if ( x < 10 ) x = 10;
-					if ( x > this.size.width - 10 ) x = this.size.width - 10;
-					if ( y < 10 ) y = 10;
-					if ( y > this.size.height - 10 ) y = this.size.height - 10;
-					w.setLocation( x, y );
+		double layoutSize = Math.min( this.size.height, this.size.width );
+		Collection<Molecule> vertices = this.graph.getVertices( );
+		for( Molecule vertex1 : vertices ) {
+			PolarPoint2D v1Location = new PolarPoint2D( this.locations.get( vertex1 ));
+			for ( Molecule vertex2 : vertices ) {
+				if ( vertex1 != vertex2 ) {
+					Point2D v2Location = this.locations.get( vertex2 );
+					double optimum = this.getOptimum( vertex1, vertex2 ) * layoutSize;
+					if ( optimum >= 0 ) {
+						v1Location.setOrigin( v2Location ); 
+						double newR = v1Location.getR( );
+						if ( newR > optimum )
+							newR -= this.attractionStep;
+							else if ( newR < optimum )
+								newR += this.repulsionStep;
+						v1Location.setLocation( newR, v1Location.getTheta( ), PolarPoint2D.POLAR );
+						v1Location.setLocation( 
+							Math.max( 10, Math.min( this.size.width - 10, v1Location.getX( ))),
+							Math.max( 10, Math.min( this.size.height - 10, v1Location.getY( ))));
+					}
 				}
 			}
+			this.setLocation( vertex1, v1Location );
 		}
 		this.currentIteration++;
 	}
@@ -99,9 +83,46 @@ public class ClusteredLayout extends AbstractLayout<Molecule,Correlation> {
 		super.setSize( size );
 		this.reset( );
 	}
+
+	/**
+	 * Returns the attraction force between two vertices. 
+	 * 
+	 * @param v1 The first vertex.
+	 * @param v2 The second vertex. 
+	 * @return The attraction force.
+	 */
+	protected double getAttraction( Molecule v1, Molecule v2 ) {
+		return (( Molecule )v1).getCorrelation( (Molecule)v2 ).getValue( ) * 0.75;
+	}
+
+	/**
+	 * Returns the repulsion force between 2 vertices. In the default implementation this
+	 * is a constant.
+	 * 
+	 * @param v1 The first vertex.
+	 * @param v2 The second vertex. 
+	 * @return The repulsion force between two vertices.
+	 */
+	protected double getRepulsion( Molecule v1, Molecule  v2 ) {
+		return 0.8;
+	}
+
+	/**
+	 * Returns the optimum distance for 2 vertices. Ideally this should be between 0
+	 * and 1, where 1 equals the minimum of the height and width of the layout.
+	 * Normally this is repulsion minus attraction, but this method can be overriden
+	 * if desired.
+	 * 
+	 * @param v1 The first vertex.
+	 * @param v2 The second vertex. 
+	 * @return The optimum distance for the 2 vertices.
+	 */
+	protected double getOptimum( Molecule v1, Molecule v2 ) {
+		return this.getRepulsion( v1, v2 ) - this.getAttraction( v1, v2 );
+	}
 	
 	/**
-	 * Calls this.initialize( )
+	 * Resets the layout to its initial state.
 	 */
 	public void reset( ) {
 		this.initialize( );
