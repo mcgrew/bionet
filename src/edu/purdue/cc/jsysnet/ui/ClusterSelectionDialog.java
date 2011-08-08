9f052119ca751eb99b295051e4ef58ae5e53a814
Thomas McGrew <tmcgrew@purdue.edu>
2011-08-08 16:27:52 -0400

    Added a missing file from a previous commit

---
+++ src/edu/purdue/cc/jsysnet/ui/ClusterSelectionDialog.java
@@ -0,0 +1,292 @@
+/*
+
+This file is part of JSysNet.
+
+JSysNet is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+JSysNet is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with JSysNet.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+package edu.purdue.cc.jsysnet.ui;
+
+import edu.purdue.bbc.util.Language;
+import edu.purdue.bbc.util.Settings;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Collection;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import javax.swing.ListSelectionModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.WindowConstants;
+
+import net.sf.javaml.clustering.KMeans;
+import net.sf.javaml.clustering.SOM;
+
+import net.sf.javaml.clustering.Clusterer;
+import net.sf.javaml.clustering.KMeans;
+import net.sf.javaml.clustering.SOM;
+
+
+public class ClusterSelectionDialog  extends JDialog implements ActionListener {
+	protected JButton clusterButton;
+	protected JButton cancelButton;
+	protected ClusterParameterPanel parameterPanel;
+	protected JComboBox clusterSelectorComboBox;
+	private Clusterer returnValue;
+	private static int hgap = 10;
+	private static int vgap = 10;
+	private boolean okPressed = false;
+
+	public ClusterSelectionDialog( Frame owner, String title ) {
+		this( owner, title, true );
+	}
+
+	public ClusterSelectionDialog( Frame owner, String title, 
+	                               boolean cancelEnabled ) {
+		super( owner, title );
+		this.setVisible( false );
+		this.getContentPane( ).setLayout( new BorderLayout( hgap, vgap ));
+		this.setBounds( Settings.getSettings( ).getInt( "windowXPosition" ),
+		                Settings.getSettings( ).getInt( "windowYPosition" ),
+		                500, 200 );
+
+		Language language = Settings.getLanguage( );
+		this.clusterButton = new JButton( language.get( "Cluster" ));
+		this.cancelButton = new JButton( language.get( "Cancel" ));
+		JPanel bottomPanel = new JPanel( new GridLayout( 1, 2, hgap, vgap ));
+		bottomPanel.add( this.clusterButton );
+		bottomPanel.add( this.cancelButton );
+		this.clusterButton.addActionListener( this );
+		this.cancelButton.addActionListener( this );
+
+		JPanel topPanel = new JPanel( new BorderLayout( hgap, vgap ));
+		JLabel clusterSelectorLabel = new JLabel( 
+			language.get( "Clustering Method" ));
+		this.parameterPanel = new SOMClusterParameterPanel( );
+		this.clusterSelectorComboBox = new JComboBox( new Object[] {
+			this.parameterPanel,
+			new KMeansClusterParameterPanel( )
+			});
+		this.clusterSelectorComboBox.addActionListener( this );
+
+		topPanel.add( clusterSelectorLabel, BorderLayout.WEST );
+		topPanel.add( this.clusterSelectorComboBox, BorderLayout.CENTER );
+
+
+		this.add( topPanel, BorderLayout.NORTH );
+		this.add( this.parameterPanel, BorderLayout.CENTER );
+		this.add( bottomPanel, BorderLayout.SOUTH );
+
+		this.addWindowListener( new WindowAdapter( ) {
+			public void windowOpened( WindowEvent e ) {
+				e.getWindow( ).pack( );
+			}
+		});
+		if ( !cancelEnabled ) {
+			this.setDefaultCloseOperation( WindowConstants.DO_NOTHING_ON_CLOSE );
+			this.cancelButton.setEnabled( false );
+		}
+		this.setModalityType( Dialog.ModalityType.APPLICATION_MODAL );
+		this.setResizable( false );
+		this.setVisible( true );
+		this.validate( );
+		this.pack( );
+	}
+
+	public Clusterer getReturnValue( ) {
+		if ( this.okPressed ) {
+			return this.parameterPanel.getClusterer( );
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Creates a new ClusterParameterDialog and returns the SampleGroups once the
+	 * selection is made.
+	 * 
+	 * @param owner The parent of this dialog.
+	 * @param title The title for the dialog.
+	 * @param samples A group of samples to be sorted into groups.
+	 */
+	public static Clusterer showInputDialog(
+			Frame owner, String title ) {
+
+		ClusterSelectionDialog dialog = 
+			new ClusterSelectionDialog( owner, title );
+		return dialog.getReturnValue( );
+	}
+
+	public void actionPerformed( ActionEvent event ) {
+		Component source = (Component)event.getSource( );
+		if ( source == this.clusterSelectorComboBox ) {
+			ClusterParameterPanel selected = 
+				(ClusterParameterPanel)((JComboBox)source).getSelectedItem( );
+			this.remove( this.parameterPanel );
+			this.parameterPanel = selected;
+			this.add( this.parameterPanel, BorderLayout.CENTER );
+			this.validate( );
+			this.pack( );
+			this.repaint( );
+		} else if ( source == this.clusterButton ) {
+			this.okPressed = true;
+			this.setVisible( false );
+		} else if ( source == this.cancelButton ) {
+			this.setVisible( false );
+		}
+	}
+
+	private abstract class ClusterParameterPanel extends JPanel {
+		protected Clusterer clusterer;
+
+		protected ClusterParameterPanel( ) {
+			super( );
+		}
+
+		protected ClusterParameterPanel( LayoutManager layout ) {
+			super( layout );
+		}
+		
+		public abstract Clusterer getClusterer( );
+	}
+
+	private class SOMClusterParameterPanel extends ClusterParameterPanel {
+		private JSpinner xDimensionSpinner;
+		private JSpinner yDimensionSpinner;
+		private JComboBox gridTypeComboBox;
+		private JSpinner iterationSpinner;
+		private JSpinner learningRateSpinner;
+		private JSpinner initialRadiusSpinner;
+		private JComboBox learningTypeComboBox;
+		private JComboBox neighborhoodFunctionComboBox;
+
+		public SOMClusterParameterPanel( ) {
+			super( new GridLayout( 8, 2, hgap, vgap ));
+			Language language = Settings.getLanguage( );
+			xDimensionSpinner    = new JSpinner( 
+				new SpinnerNumberModel( 5, 0, 25, 1 ));
+			yDimensionSpinner    = new JSpinner( 
+				new SpinnerNumberModel( 5, 0, 25, 1 ));
+			iterationSpinner     = new JSpinner( 
+				new SpinnerNumberModel( 10000, 0, 100000, 5000 ));
+			learningRateSpinner  = new JSpinner( 
+				new SpinnerNumberModel( 0.1, 0, 1, 0.1 ));
+			initialRadiusSpinner = new JSpinner( 
+				new SpinnerNumberModel( 10, 0, 25, 1 ));
+			this.gridTypeComboBox = new JComboBox( new Object[] {
+				SOM.GridType.HEXAGONAL,
+				SOM.GridType.RECTANGLES
+			});
+			this.learningTypeComboBox = new JComboBox( new Object[] {
+				SOM.LearningType.EXPONENTIAL,
+				SOM.LearningType.INVERSE,
+				SOM.LearningType.LINEAR
+			});
+			this.neighborhoodFunctionComboBox = new JComboBox( new Object[] {
+				SOM.NeighbourhoodFunction.GAUSSIAN,
+				SOM.NeighbourhoodFunction.STEP
+			});
+			this.add( new JLabel( language.get( "X axis dimensions" )));
+			this.add( this.xDimensionSpinner );
+			this.add( new JLabel( language.get( "Y axis dimensions" )));
+			this.add( this.yDimensionSpinner );
+			this.add( new JLabel( language.get( "Grid type" )));
+			this.add( this.gridTypeComboBox );
+			this.add( new JLabel( language.get( "Number of iterations" )));
+			this.add( this.iterationSpinner );
+			this.add( new JLabel( language.get( "Learning rate" )));
+			this.add( this.learningRateSpinner );
+			this.add( new JLabel( language.get( "Initial Radius" )));
+			this.add( this.initialRadiusSpinner );
+			this.add( new JLabel( language.get( "Learning type" )));
+			this.add( this.learningTypeComboBox );
+			this.add( new JLabel( language.get( "Neighborhood function" )));
+			this.add( this.neighborhoodFunctionComboBox );
+		}
+
+		public Clusterer getClusterer( ) {
+			return new SOM(
+				// number of dimensions on the x axis
+				Integer.parseInt( this.xDimensionSpinner.getValue( ).toString( )),
+				// number of dimensions on the y axis
+				Integer.parseInt( this.yDimensionSpinner.getValue( ).toString( )),
+				// type of grid.
+				(SOM.GridType)this.gridTypeComboBox.getSelectedItem( ),
+				// number of iterations
+				Integer.parseInt( this.iterationSpinner.getValue( ).toString( )),
+				// learning rate of algorithm
+				Double.parseDouble( this.learningRateSpinner.getValue( ).toString( )),
+				// initial radius
+				Integer.parseInt( this.initialRadiusSpinner.getValue( ).toString( )),
+				// type of learning to use
+				(SOM.LearningType)this.learningTypeComboBox.getSelectedItem( ),
+				// neighborhood function.
+				(SOM.NeighbourhoodFunction)
+					this.neighborhoodFunctionComboBox.getSelectedItem( )
+			);
+		}
+
+		public String toString( ) {
+			return "Self Organizing Map";
+		}
+	}
+
+	private class KMeansClusterParameterPanel extends ClusterParameterPanel {
+		private JSpinner clustersSpinner;
+		private JSpinner iterationsSpinner;
+
+		public KMeansClusterParameterPanel( ) {
+			super( new GridLayout( 2, 2, hgap, vgap ));
+			Language language = Settings.getLanguage( );
+			clustersSpinner    = new JSpinner( 
+				new SpinnerNumberModel( 5, 0, 25, 1 ));
+			iterationsSpinner    = new JSpinner( 
+				new SpinnerNumberModel( 5, 0, 25, 1 ));
+			this.add( new JLabel( language.get( "Number of Clusters" )));
+			this.add( this.clustersSpinner );
+			this.add( new JLabel( language.get( "Number of Iterations" )));
+				this.add( this.iterationsSpinner );
+		}
+
+		public Clusterer getClusterer( ) {
+			return new KMeans(
+				// number of clusters
+				Integer.parseInt( this.clustersSpinner.getValue( ).toString( )),
+				// number of iterations
+				Integer.parseInt( this.iterationsSpinner.getValue( ).toString( ))
+			);
+		}
+
+		public String toString( ) {
+			return "K-Means";
+		}
+	}
+}
+
