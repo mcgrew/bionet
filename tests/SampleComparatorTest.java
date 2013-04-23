ff9608316224bd0a6c4d539c7606c1df1456ccc7
Thomas McGrew <tmcgrew@purdue.edu>
2013-04-23 13:00:04 -0400

    Updated bbc-util to the latest version
    Updated bbc-util to the latest version, which includes a bugfix for some
      statistical functions
    Added tests for Sample and SampleComparator
    Updated the ant build script to remove a warning in ant 1.7

--- tests/SampleComparatorTest.java
+++ tests/SampleComparatorTest.java
@@ -1,3 +1,22 @@
+/*
+
+This file is part of BioNet.
+
+BioNet is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BioNet is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BioNet.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 
 import edu.purdue.cc.bionet.util.Sample;
 import edu.purdue.cc.bionet.util.SampleComparator;
